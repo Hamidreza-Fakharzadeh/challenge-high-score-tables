9558d88e103772135f56a6b2389c7a671ddb3e0a
hamidreza <hrfarzadeh@gmail.com>
2020-05-27 01:22:16 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import React from 'react';
-import './App.css';
+
 import AllCountryScores from './AllCountryScores';
+import './App.css';
+import './grid.css';
 
 function App() {
   let arrOfObj = AllCountryScores()
  console.log(arrOfObj)
   return (
-    <div className="App">
-        <p>High scores per country</p>
+    <div className="App container">
+      <div className ="main-div row">
         
+        <div className ="table-score lg-col-12 md-col-12">
+        <h1 className ="heading">High scores per country</h1>
           {arrOfObj.map((obj,index) => {
             return (
-              <table key = {index}>
-                <thead>
-                <th>{"HIGH SCORES: " + obj.name}</th>
-                </thead>
-                <tbody>
-                {obj.scores.map((scorObj,index2) => {
-                    return (
-                      <tr key = {index2}>
-                        <td>{scorObj.n}</td>
-                        <td>{scorObj.s}</td>
-                      </tr>
-                          )
+              <div key = {index} className ="body-score">
+                  <h2 className ="country">{"HIGH SCORES: " + obj.name}</h2>
+                  
+                    {obj.scores.map((scorObj,index2) => {
+                        return (
+                          <div key = {index2} className ="last-div row">
+                            <div className = "last-child1 lg-col-6 md-col-6">{scorObj.n}</div>
+                            <div className = "last-child2 lg-col-6 md-col-6" >{scorObj.s}</div>
+                          </div>
+                              )
                       })
                 }
-                </tbody>
-              </table>
-              
+            </div>
             )
            })
           }
-          
-        
+          </div>
+          </div> 
     </div>
   );
 }
